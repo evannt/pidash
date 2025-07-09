9b4eb5aa45188cf07808512844aa1af4800dffec
Evan Thompson <evanthompson@wireless-10-104-137-252.umd.edu>
2025-07-09 16:35:38 -0400

    Fix Initial Bugs
    Cleanup

--- src/config.py
+++ src/config.py
@@ -12,8 +12,6 @@ class Config:
     
     config_file = os.path.join(BASE_DIR, "config", "device.json")
 
-    #current_image_file = os.path.join(BASE_DIR, "static", "images")
-
     def __init__(self):
         self.config = self.load_config()
     
