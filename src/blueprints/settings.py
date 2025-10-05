1a276dfab2c0c128f6fa732c6d54505104a34e1f
Evan <107281512+evannt@users.noreply.github.com>
2025-10-05 19:04:39 -0400

    Prevent Display Update After Image Removal
    If the current image is not removed, the display is not updated

--- src/blueprints/settings.py
+++ src/blueprints/settings.py
@@ -8,8 +8,8 @@ bp = Blueprint("settings", __name__)
 @bp.route("/settings")
 def home():
     cfg = current_app.config["config"]
-    refresh_interval = cfg.get("refresh_interval", 900)
-    orientation = cfg.get("orientation", "landscape")
+    refresh_interval = cfg.get("refresh_interval")
+    orientation = cfg.get("orientation")
 
     return render_template("settings.html",
                            refresh_interval=refresh_interval,
